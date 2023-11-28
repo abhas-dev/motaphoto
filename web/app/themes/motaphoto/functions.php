c71e7ab3ea856971a8b2a96a54072f9c85b993a2
Abdelatif H <ab.hassaine@protonmail.com>
2023-11-28 11:41:29 +0100

    Update readme

--- web/app/themes/motaphoto/functions.php
+++ web/app/themes/motaphoto/functions.php
@@ -9,7 +9,7 @@ function motaphoto_enqueue_styles() {
 	// Stylesheets
 	wp_enqueue_style('motaphoto-style', get_theme_file_uri('/public/css/main.css'));
 	// Scripts
-	wp_enqueue_script('child-script', get_theme_file_uri('/build/index.js'), NULL, '1.0', $args = ['strategy' => 'defer', 'in_footer' => true]);
+	wp_enqueue_script('motaphoto-scripts', get_theme_file_uri('/public/build/index.js'), NULL, '1.0', $args = ['strategy' => 'defer', 'in_footer' => true]);
 }
 
 add_action('wp_enqueue_scripts', 'motaphoto_enqueue_styles');
