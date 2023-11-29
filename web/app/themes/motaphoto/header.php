602276f6d28ed051a31a427db1e19cb95d289eb2
Abdelatif H <ab.hassaine@protonmail.com>
2023-11-29 22:31:59 +0100

    Add contact-form-7 package + some of metas in header

--- web/app/themes/motaphoto/header.php
+++ web/app/themes/motaphoto/header.php
@@ -1,14 +1,14 @@
 <!doctype html>
-<html lang="fr">
+<html <?php language_attributes();  ?>>
 <head>
     <?php wp_head(); ?>
-	<meta charset="UTF-8">
+	<meta charset="<?php bloginfo('charset'); ?>">
 	<meta name="viewport"
 	      content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
 	<title>Document</title>
 </head>
-<body>
+<body <?php body_class(); ?>>
 	<header>
 		<div class="container">
 			<div class="header__logo">
